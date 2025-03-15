854043d44cc0e01bfa095ce768fc7d1b24bd4a31
asadi bharadwaj <asadibharadwaj@Bharadwajs-Macbook-Air.local>
2025-03-15 14:22:40 +0530

    Added 'create' api and constant file

--- Accounts/src/main/java/com/UdemyCource/Accounts/Controller/AccountsController.java
+++ Accounts/src/main/java/com/UdemyCource/Accounts/Controller/AccountsController.java
@@ -1,13 +1,27 @@
 package com.UdemyCource.Accounts.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.UdemyCource.Accounts.Constants.AccountsConstants;
+import com.UdemyCource.Accounts.dto.CustomerDto;
+import com.UdemyCource.Accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/api")
 public class AccountsController {
 
     @GetMapping("/test")
     public String sayHello(){
         return "Hello World";
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+    }
+
 }
