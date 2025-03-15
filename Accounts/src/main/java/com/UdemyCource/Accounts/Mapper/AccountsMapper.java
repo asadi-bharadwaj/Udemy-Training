ce6c1aca78ef7bb4b75ab2e44922218a8bb6e70a
asadi bharadwaj <asadibharadwaj@Bharadwajs-Macbook-Air.local>
2025-03-15 14:42:34 +0530

    mapper logic added

---
+++ Accounts/src/main/java/com/UdemyCource/Accounts/Mapper/AccountsMapper.java
@@ -0,0 +1,21 @@
+package com.UdemyCource.Accounts.Mapper;
+
+import com.UdemyCource.Accounts.Entity.Accounts;
+import com.UdemyCource.Accounts.dto.AccountsDto;
+
+public class AccountsMapper {
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto){
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts){
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+}
